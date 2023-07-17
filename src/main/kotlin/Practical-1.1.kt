0dcdf3f7804c73260fdb4c538a79fb52f1030c12
Darshil <darshilchodvadiya193@gmail.com>
2023-07-17 22:35:13 +0530

    Practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println("Hello World")
+
+    var i:Int=16
+    var f:Float=16.23F
+    var c:Char='D'
+    var s:String="Darshil"
+    var b:Boolean= true
+    var d:Double=16.35
+    var l:Long= 21012021016
+    var sh:Short= -16
+    var by:Byte= 123
+
+    println("Integer Value: $i")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $s")
+    println("Boolean Value: $b")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $sh")
+    println("Byte Value: $by")
+
+
+}
